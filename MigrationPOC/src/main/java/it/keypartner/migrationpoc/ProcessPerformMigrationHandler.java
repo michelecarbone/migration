ca676ed67d3aa8d0cb467c1e0be5d182725b0582
Giovanni Mezzullo <giovanni.mezzullo@keypartner.it>
2016-08-22 16:12:32 +0200

    Add old ref

--- MigrationPOC/src/main/java/it/keypartner/migrationpoc/ProcessPerformMigrationHandler.java
+++ MigrationPOC/src/main/java/it/keypartner/migrationpoc/ProcessPerformMigrationHandler.java
@@ -7,12 +7,20 @@ import javax.persistence.EntityManagerFactory;
 
 import org.drools.core.command.impl.CommandBasedStatefulKnowledgeSession;
 import org.drools.core.command.impl.KnowledgeCommandContext;
+import org.drools.core.common.InternalKnowledgeRuntime;
 import org.jbpm.process.audit.JPAAuditLogService;
 import org.jbpm.process.audit.ProcessInstanceLog;
 import org.jbpm.runtime.manager.impl.jpa.EntityManagerFactoryManager;
-import org.jbpm.workflow.instance.WorkflowProcessInstanceUpgrader;
+import org.jbpm.workflow.core.NodeContainer;
+import org.jbpm.workflow.core.impl.NodeImpl;
+import org.jbpm.workflow.instance.NodeInstanceContainer;
+import org.jbpm.workflow.instance.impl.NodeInstanceImpl;
+import org.jbpm.workflow.instance.impl.WorkflowProcessInstanceImpl;
+import org.kie.api.definition.process.Node;
+import org.kie.api.definition.process.WorkflowProcess;
 import org.kie.api.runtime.KieRuntime;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.process.NodeInstance;
 import org.kie.api.runtime.process.ProcessInstance;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
@@ -84,8 +92,12 @@ public class ProcessPerformMigrationHandler implements WorkItemHandler {
 			KieSession tobe = JPAKnowledgeService.newStatefulKnowledgeSession(
 					toBeManager.getEnvironment().getKieBase(), null, toBeManager.getEnvironment().getEnvironment());
 
-			WorkflowProcessInstanceUpgrader.upgradeProcessInstance(tobe, Long.parseLong(in_fromProcessInstaceId),
-					in_toProcessId, null);
+			// WorkflowProcessInstanceUpgrader.upgradeProcessInstance(tobe,
+			// Long.parseLong(in_fromProcessInstaceId),
+			// in_toProcessId, null);
+
+			upgradeProcessInstance(extractIfNeeded(current), extractIfNeeded(tobe),
+					Long.valueOf(in_fromProcessInstaceId), in_toProcessId, null);
 
 			log.info("Migration From ProcessInstance [" + in_fromProcessInstaceId + "] To ProcessID [" + in_toProcessId
 					+ "]");
@@ -104,6 +116,37 @@ public class ProcessPerformMigrationHandler implements WorkItemHandler {
 		return outcomeBuffer.toString();
 	}
 
+	private static void upgradeProcessInstance(KieRuntime oldkruntime, KieRuntime kruntime, long processInstanceId,
+			String processId, Map<String, String> nodeMapping) {
+		if (nodeMapping == null) {
+			nodeMapping = new HashMap<String, String>();
+		}
+		WorkflowProcessInstanceImpl processInstance = (WorkflowProcessInstanceImpl) oldkruntime
+				.getProcessInstance(processInstanceId);
+		if (processInstance == null) {
+			throw new IllegalArgumentException("Could not find process instance " + processInstanceId);
+		}
+		if (processId == null) {
+			throw new IllegalArgumentException("Null process id");
+		}
+		WorkflowProcess process = (WorkflowProcess) kruntime.getKieBase().getProcess(processId);
+		if (process == null) {
+			throw new IllegalArgumentException("Could not find process " + processId);
+		}
+		if (processInstance.getProcessId().equals(processId)) {
+			return;
+		}
+		synchronized (processInstance) {
+			org.kie.api.definition.process.Process oldProcess = processInstance.getProcess();
+			processInstance.disconnect();
+			processInstance.setProcess(oldProcess);
+			updateNodeInstances(processInstance, nodeMapping, (NodeContainer) process);
+			processInstance.setKnowledgeRuntime((InternalKnowledgeRuntime) kruntime);
+			processInstance.setProcess(process);
+			processInstance.reconnect();
+		}
+	}
+
 	private KieRuntime extractIfNeeded(KieSession ksession) {
 		if (ksession instanceof CommandBasedStatefulKnowledgeSession) {
 			return ((KnowledgeCommandContext) ((CommandBasedStatefulKnowledgeSession) ksession).getCommandService()
@@ -111,6 +154,53 @@ public class ProcessPerformMigrationHandler implements WorkItemHandler {
 		}
 
 		return ksession;
+	}
+
+	private static void updateNodeInstances(NodeInstanceContainer nodeInstanceContainer,
+			Map<String, String> nodeMapping, NodeContainer nodeContainer) {
+		if (nodeMapping == null || nodeMapping.isEmpty()) {
+			return;
+		}
+		for (NodeInstance nodeInstance : nodeInstanceContainer.getNodeInstances()) {
+			String oldNodeId = (String) ((NodeImpl) ((org.jbpm.workflow.instance.NodeInstance) nodeInstance).getNode())
+					.getMetaData().get("UniqueId");
+			String newNodeId = nodeMapping.get(oldNodeId);
+			if (newNodeId == null) {
+				continue;
+			}
+			Long upgradedNodeId = findNodeIfByUniqueId(newNodeId, nodeContainer);
+			if (upgradedNodeId == null) {
+				try {
+					upgradedNodeId = Long.parseLong(newNodeId);
+				} catch (NumberFormatException e) {
+					continue;
+				}
+			}
+
+			((NodeInstanceImpl) nodeInstance).setNodeId(upgradedNodeId);
+			if (nodeInstance instanceof NodeInstanceContainer) {
+				updateNodeInstances((NodeInstanceContainer) nodeInstance, nodeMapping, nodeContainer);
+			}
+		}
+
+	}
+
+	private static Long findNodeIfByUniqueId(String uniqueId, NodeContainer nodeContainer) {
+		Long result = null;
+
+		for (Node node : nodeContainer.getNodes()) {
+			if (uniqueId.equals(node.getMetaData().get("UniqueId"))) {
+				return node.getId();
+			}
+			if (node instanceof NodeContainer) {
+				result = findNodeIfByUniqueId(uniqueId, (NodeContainer) node);
+				if (result != null) {
+					return result;
+				}
+			}
+		}
+
+		return result;
 	}
 
 	public boolean validate(String in_fromDepoymentId, String in_toDepoymentId, String in_fromProcessInstaceId,
