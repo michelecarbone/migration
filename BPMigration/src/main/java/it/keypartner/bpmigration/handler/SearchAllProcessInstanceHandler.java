347901457679d0a1ffed0b5850e0a5010bf84a42
Giovanni Mezzullo <giovanni.mezzullo@keypartner.it>
2016-08-23 18:02:18 +0200

    Handler to search all process instance

---
+++ BPMigration/src/main/java/it/keypartner/bpmigration/handler/SearchAllProcessInstanceHandler.java
@@ -0,0 +1,21 @@
+package it.keypartner.bpmigration.handler;
+
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+
+public class SearchAllProcessInstanceHandler implements WorkItemHandler {
+
+	@Override
+	public void executeWorkItem(WorkItem arg0, WorkItemManager arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void abortWorkItem(WorkItem arg0, WorkItemManager arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
