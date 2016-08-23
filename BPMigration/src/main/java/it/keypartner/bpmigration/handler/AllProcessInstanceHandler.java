f620ba39bc8bb2e575fda035ec4f2b612986a2a4
Giovanni Mezzullo <giovanni.mezzullo@keypartner.it>
2016-08-23 18:41:20 +0200

    Change name

--- BPMigration/src/main/java/it/keypartner/bpmigration/handler/AllProcessInstanceHandler.java
+++ BPMigration/src/main/java/it/keypartner/bpmigration/handler/AllProcessInstanceHandler.java
@@ -3,11 +3,15 @@ package it.keypartner.bpmigration.handler;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class SearchAllProcessInstanceHandler implements WorkItemHandler {
+public class AllProcessInstanceHandler implements WorkItemHandler {
+
+	private static final Logger log = LoggerFactory.getLogger(AllProcessInstanceHandler.class);
 
 	@Override
-	public void executeWorkItem(WorkItem arg0, WorkItemManager arg1) {
+	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
 		// TODO Auto-generated method stub
 
 	}
