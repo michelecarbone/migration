c7e33dda9e27e99e6994433be47ff7fc886b7c1f
Giovanni Mezzullo <giovanni.mezzullo@keypartner.it>
2016-08-24 16:52:47 +0200

    Add Migration Handler

---
+++ BPMigration/src/main/java/it/keypartner/bpmigration/handler/PerformMigrationHandler.java
@@ -0,0 +1,42 @@
+package it.keypartner.bpmigration.handler;
+
+import it.keypartner.bpmigration.ProcessToMigrate;
+import it.keypartner.bpmigration.dao.MigrationManager;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PerformMigrationHandler implements WorkItemHandler {
+
+	private static final Logger logger = LoggerFactory.getLogger(MigrationManager.class);
+
+	@SuppressWarnings("unchecked")
+	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+		List<ProcessToMigrate> processToMigrateList = (List<ProcessToMigrate>) workItem
+				.getParameter("in_processToMigrateList");
+		StringBuilder stringBuilder = new StringBuilder();
+		for (ProcessToMigrate processToMigrate : processToMigrateList) {
+			logger.info("Start Migration for -> " + processToMigrate);
+			String outcome = MigrationManager.migrate(processToMigrate);
+			logger.info("End Migration for -> " + processToMigrate);
+			stringBuilder.append(outcome);
+		}
+		Map<String, Object> results = new HashMap<String, Object>();
+		results.put("out_outcome", stringBuilder);
+
+		manager.completeWorkItem(workItem.getId(), results);
+	}
+
+	public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+		// no-op
+
+	}
+
+}
