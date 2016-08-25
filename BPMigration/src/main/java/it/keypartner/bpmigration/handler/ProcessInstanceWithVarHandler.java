927561ec22822b8d820b8d0e88e2c63f815ba8e3
Giovanni Mezzullo <giovanni.mezzullo@keypartner.it>
2016-08-25 09:44:12 +0200

    proc

---
+++ BPMigration/src/main/java/it/keypartner/bpmigration/handler/ProcessInstanceWithVarHandler.java
@@ -0,0 +1,53 @@
+package it.keypartner.bpmigration.handler;
+
+import it.keypartner.bpmigration.ProcessVarToMigrate;
+import it.keypartner.bpmigration.SearchProcessInstance;
+import it.keypartner.bpmigration.builder.ProcessToMigrateBuilder;
+import it.keypartner.bpmigration.dao.ProcessManageDAO;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jbpm.process.audit.ProcessInstanceLog;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProcessInstanceWithVarHandler implements WorkItemHandler {
+
+	private static final Logger log = LoggerFactory.getLogger(ProcessInstanceWithVarHandler.class);
+
+	@Override
+	public void executeWorkItem(WorkItem workItem, WorkItemManager wkManager) {
+		log.info("Calling search ...");
+
+		SearchProcessInstance searchProcessInstance = (SearchProcessInstance) workItem.getParameter("in_search");
+		log.info("Params -> " + searchProcessInstance.toString());
+
+		ProcessManageDAO processManageDAO = new ProcessManageDAO();
+		List<ProcessInstanceLog> processFound = processManageDAO.retriveActiveProcessInstanceWithVar(
+				searchProcessInstance.getFromDeploymentId(), searchProcessInstance.getFromProcessId(),
+				searchProcessInstance.getSearchProcessVarName(), searchProcessInstance.getSearchProcessVarValue());
+		List<org.kie.api.runtime.manager.audit.VariableInstanceLog> variableInstanceLogs = processManageDAO
+				.getVariableInstanceLogsList();
+
+		// Invoco il builder per costruire le info da estrarre
+		List<ProcessVarToMigrate> processToMigrates = ProcessToMigrateBuilder.build(processFound, variableInstanceLogs,
+				searchProcessInstance);
+		log.info("Process To Migrate [" + processToMigrates.size() + "]");
+
+		Map<String, Object> results = new HashMap<String, Object>();
+		results.put("out_processList", processToMigrates);
+
+		wkManager.completeWorkItem(workItem.getId(), results);
+	}
+
+	@Override
+	public void abortWorkItem(WorkItem arg0, WorkItemManager arg1) {
+
+	}
+
+}
