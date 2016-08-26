615247235988d78826da56cbe88ed53a8723e070
Giovanni Mezzullo <giovanni.mezzullo@keypartner.it>
2016-08-26 12:46:49 +0200

    ewww

--- BPMigration/src/main/java/it/keypartner/bpmigration/handler/ValidateParmsHandler.java
+++ BPMigration/src/main/java/it/keypartner/bpmigration/handler/ValidateParmsHandler.java
@@ -39,7 +39,7 @@ public class ValidateParmsHandler implements WorkItemHandler {
 
 		log.info("Result of Validation [" + valid + "]");
 		Map<String, Object> results = new HashMap<String, Object>();
-		results.put("out_validMsg", "fff");
+		results.put("out_validMsg", valid);
 		wkManager.completeWorkItem(workItem.getId(), results);
 	}
 
